f65864ba4eb89c218c132b9339dd5a1a62cd8ee8
unknown <christianmolicki@gmail.com>
2023-08-15 20:22:13 +0200

    forgor4

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class ytdownload:
         if video and audio:
             logging.info('successfully downloaded both, merging now')
             try:
-                result = await asyncio.create_subprocess_exec(f'ffmpeg -i tempvideo.{videoextension} -i tempaudio.{audioextension} -v quiet -c:v copy {"-c:a copy " if videoextension == audioextension else ""} -map 0:v:0 -map 1:a:0 -y merged.{videoextension}')
+                result = await asyncio.create_subprocess_exec(*(f'ffmpeg -i tempvideo.{videoextension} -i tempaudio.{audioextension} -v quiet -c:v copy {"-c:a copy " if videoextension == audioextension else ""} -map 0:v:0 -map 1:a:0 -y merged.{videoextension}'.split()))
             except Exception as e:
                 print(e)
                 print(result.stdout)
