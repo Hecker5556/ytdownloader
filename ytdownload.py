f5ad9d1788ab75c852a242b398b1c4ea5767017a
unknown <christianmolicki@gmail.com>
2024-10-04 18:56:07 +0200

    truncate filename under 255 characters

--- ytdownload.py
+++ ytdownload.py
@@ -819,7 +819,10 @@ class ytdownload:
             os.mkdir(self.title)
         clear = lambda x: "".join([i for i in x if i not in "\\/:*?<>|()\""])
         give_file = lambda folder, title, date, ext: f"{clear(folder)}/"+clear(title)+date+f".{ext}" if folder else clear(title)+date+f".{ext}"
-        resultfile = give_file(self.title, self.all_formats['misc'].get('title') if not self.filename else self.filename, "", self.ext)
+        title = self.filename if self.filename else self.all_formats['misc'].get('title')
+        if len(title.encode()) > 252:
+            title = title.encode()[:252].decode()
+        resultfile = give_file(self.title, title, "", self.ext)
         try:
             os.rename(self.result_file, resultfile)
         except FileExistsError:
